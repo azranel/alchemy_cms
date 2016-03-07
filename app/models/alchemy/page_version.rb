d3a41af1b439819c4999bd4abe9e050f0937a03f
Thomas von Deyen <tvd@bitspire.de>
2016-03-07 23:22:21 +0100

    Associate elements with page version
    Element do not belong to a page anymore, instead they belong to a version of a page.
    
    Therefore lots of page elements relations needed to be rewritten to be either through page's public_version or current_version.
    
    Page#elements has been aliased as public_elements to not break existing sites.

--- app/models/alchemy/page_version.rb
+++ app/models/alchemy/page_version.rb
@@ -1,8 +1,13 @@
 module Alchemy
   class PageVersion < ActiveRecord::Base
+
     belongs_to :page,
       class_name: 'Alchemy::Page',
       inverse_of: :versions,
       required: true
+
+    has_many :elements, -> { order(:position) },
+      class_name: 'Alchemy::Element',
+      inverse_of: :page_version
   end
 end
